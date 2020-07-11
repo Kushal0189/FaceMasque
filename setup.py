21b50d18e87886fa7d416b40c01f4a99636d3cf2
Kushal Master <46081113+Kushal0189@users.noreply.github.com>
2020-07-11 18:41:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="FaceMasque",
-    version="1.0",
+    version="1.1",
     description="A Python package to classify weather a person is weared a mask or not.",
     long_description=readme(),
     long_description_content_type="text/markdown",
