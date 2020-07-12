da1beb856ecd827c153fdcbc7f36c586c22dd6b8
kushal master <kushalmaster8@gmail.com>
2020-07-12 12:07:56 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Detection-of-Person-With-or-Without-Mask
+
+This project is mainly made for detecting a person and classify weather a person is weared the mask or not.
+
+To use this project simply folloe folloeing steps.
+
+1) Use following command in your working directory through type it in command Prompt.
+   pip install FaceMasque
+ 
+2) Now, import it in your python file.
+   import FaceMasque
+ 
+3) Simply call folloeing function and you are ready with mask detection project. 
+   detected_image = FaceMasque.mask_detection(original_image)
+   
---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+def readme():   
+    with open('README.md', 'r') as f:
+        README = f.read()
+    return README
+
+
+setuptools.setup(
+    name="FaceMasque-Kushal0189",
+    version="1.0",
+    description="A Python package to classify weather a person is weared a mask or not.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kushal0189/FaceMasque",
+    author="KUSHAL MASTER",
+    author_email="kushalmaster8@gmail.com",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "onnx==1.6.0",
+        "onnx-tf==1.3.0",
+        "onnxruntime==0.5.0",
+        "opencv-python==4.1.1.26",
+        "tensorflow==1.15.2",
+        "keras",
+    ],
+    python_requires='>=3.6',
+)
