22b82d49717fd8ed530f8a8c1b02edb9b0444fa2
Kushal Master <46081113+Kushal0189@users.noreply.github.com>
2020-09-23 11:32:23 +0530

    final commit

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setuptools.setup(
     name="FaceMasque",
-    version="1.0.5",
+    version="1.0.6",
     description="A Python package to classify weather a person is weared a mask or not.",
     long_description=readme(),
     long_description_content_type="text/markdown",
